5f999a7353821ae91db1ed8c2dc27adba4c6aaed
pepitan <drevxluv2@gmail.com>
2017-06-13 12:32:55 +0700

    Second Commit

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl221/umovie/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl221/umovie/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,8 +14,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import id.sch.smktelkom_mlg.privateassignment.xirpl221.umovie.adapter.ComingSoonAdapter;
+import id.sch.smktelkom_mlg.privateassignment.xirpl221.umovie.adapter.TopAdapter;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, TopAdapter.ISourceAdapter, ComingSoonAdapter.IComingSoonAdapter {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,25 +81,28 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            fragment = new TopFragment();
+            setTitle("Top Playing");
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            fragment = new SoonFragment();
+            setTitle("Now Playing");
         }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment).commitNow();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void showArticles(String title, String overview, String poster_path) {
+
     }
 }
